c360773efc5a796794a1e85cab4b6d3e2d265cfb
esaucarba99-lgtm <esaucarba99@gmail.com>
2026-03-22 17:07:52 -0600

    Add files via upload

---
+++ sorpresa.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Título principal de la página
+st.title("¡Sorpresa! 🎉")
+
+# Mensaje en pantalla
+st.write("¡Hola! Has abierto este programa directamente desde un link.")
+
+# Animación de globos
+st.balloons()
